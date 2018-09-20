cd9dabbd31e72cdbc47efe5f2f02ff76db5dc1f6
Drakonkinst <drakonkinstmc@gmail.com>
2018-09-20 16:06:55 -0700

    Update button.js description
    - Added better comments
    - Changed the no event on button click warning to have quotes around the button's id

--- script/button.js
+++ script/button.js
@@ -1,10 +1,17 @@
 /*
- * property values:
+ * Button class that represents a clickable button in the window.
+ * Buttons can have text inside of them, a cooldown before it can be
+ * clicked again, an action when it is clicked, and an action after the
+ * button has cooled down.
+ * 
+ * properties:
  * - String id: button's id (default "btn_undefined")
  * - String text: text inside button element (default "press me")
  * - int cooldown: cooldown of button in milliseconds (default 1000)
- * - Tooltip tooltip: button tooltip (optional)
- * - int width: special width of button (optional)
+ * - Function onClick: function that runs when the button is clicked (default empty)
+ * - Function onFinish: function that runs when the button finished cooling down (default empty)
+ * - (optional) Tooltip tooltip: button tooltip
+ * - (optional) int width: special width of button
  * */
 function Button(properties) {
     this.id = properties.id || "btn_undefined";
@@ -19,8 +26,11 @@ function Button(properties) {
         .click(function() {
             var el = $(this);
             if(!el.hasClass("disabled")) {
-                var button = Buttons.getButton($(el).attr("id"));
+                //uses a callback to find the button object from the element's id, then clicks it
+                var button = Buttons.getButton(el.attr("id"));
                 var result = button.onClick();
+
+                //if result does not exist or returns true, begins the button cooldown
                 if(isUndefined(result) || result) {
                     button.startCooldown();
                 }
@@ -41,19 +51,23 @@ function Button(properties) {
         this.element.css("width", properties.width)
     }
 
+    //every button should do something on click, so warns console if nothing happens
     this.onClick = properties.onClick || function() {
-        Logger.warn("Button " + this.id + " clicked but nothing happened");
+        Logger.warn("Button \"" + this.id + "\" clicked but nothing happened");
     };
     this.onFinish = properties.onFinish || function() {
         //empty function
     };
     
+    //registers button for easier callbacks later
     Buttons.buttons[this.id] = this;
 }
 Button.prototype = {
     //initiates button cooldown, called on click
     startCooldown: function() {
         var cooldown = this.cooldown;
+
+        //button hax!
         if(Game.options.instantButtons) {
             cooldown = 100;
         } else if(Game.options.fastButtons) {
@@ -64,9 +78,11 @@ Button.prototype = {
             return;
         }
 
+        //makes sure the button isn't already in the middle of a cooldown
         this.clearCooldown();
 
         this.element.find(".cooldown").width("100%").animate({width:"0%"}, cooldown, "linear", function() {
+            //callbacks are fun
             var button = Buttons.getButton($(this).parent().attr("id"));
             button.onFinish();
             button.clearCooldown();
@@ -106,8 +122,16 @@ Button.prototype = {
     }
 };
 
+/* 
+ * Buttons object that stores and handles requests for all
+ * Button objects, allows for easier callbacks and less headaches
+ * */
 var Buttons = {
+    //keymap of all button ids and their associated buttons
+    //TODO: each module should have their own button keymap
     buttons: {},
+
+    //returns a Button object based on requested id
     getButton: function(id) {
         return Buttons.buttons[id];
     }
