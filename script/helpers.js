a117e56b79077c6b3459e3a47db6301ce057d2ef
Drakonkinst <drakonkinstmc@gmail.com>
2018-09-19 20:11:22 -0700

    Added comments for helpers.js and logger.js
    Updated helper.js and logger.js to have better comments and organization.

--- script/helpers.js
+++ script/helpers.js
@@ -1,45 +1,68 @@
- /* ====== HELPER METHODS ====== */
+/*
+ * Miscellaneous helper functions.
+ * Last edited: 9/19/2018 by Drakonkinst
+ * */
 
- //return if the VARIABLE's type matches the TYPE requested
- function isType(variable, type) {
+//returns whether a variable's type matches the requested type
+function isType(variable, type) {
     if(type === "array") {
         return Array.isArray(variable);
     }
     return typeof variable === type;
 }
 
+//returns a random number between min and max
 function randNum(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-//returns a random value from an ARRAY
+//returns a randomly selected item from an array
 function chooseRandom(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-function chooseWeighted(choices) {
-    var totalWeight = 0;
-    var weightSum = 0;
+/*
+ * Returns an object based on its weight in the given keymap.
+ * 
+ * For example, in keymap {a: 1, b: 1, c: 3, d: 1} a, b, and d
+ * have the same chance of being selected, while c has triple of
+ * their individual chance. This percentage-based probability
+ * makes it easy to create trees of probability without
+ * massive calculations, since all of the values are relative.
+ * */
+function chooseWeighted(choiceMap) {
 
-    for(var i in choices) {
-        if(choices.hasOwnProperty(i)) {
-            totalWeight += parseFloat(choices[i]);
+    //first, find the sum of all the values in choiceMap
+    var totalWeight = 0;
+    
+    for(var i in choiceMap) {
+        if(choiceMap.hasOwnProperty(i)) {
+            totalWeight += parseFloat(choiceMap[i]);
         }
     }
 
+    /*
+     * Chooses a random number based on totalWeight, then
+     * uses weightSum to find which choice interval that number lies in.
+     * 
+     * Larger weights will result in larger intervals, and therefore
+     * have a greater chance of being selected.
+     * */
     var rand = randNum(0, totalWeight);
+    var weightSum = 0;
 
-    for(i in choices) {
-        weightSum += parseFloat(choices[i]);
+    for(i in choiceMap) {
+        weightSum += parseFloat(choiceMap[i]);
         if(rand < weightSum) {
             return i;
         }
     }
 
+    //this code should be unreachable
     Logger.warn("No choice found");
 }
 
-//returns if an OBJECT has no properties
+//returns if the object has no properties
 function isEmpty(object) {
 	for(var key in object) {
 		if(object.hasOwnProperty(key)) {
--- script/logger.js
+++ script/logger.js
@@ -1,32 +1,56 @@
-/* ====== LOGGER OBJECT ====== 
- * Wraps the console so that debug messages
- * are only printed if Game.options.debug
- * is true.
- */
+/* 
+ * Logger object that wraps the default console so that
+ * debug messages are only printed if Logger.isDebug()
+ * returns true.
+ * Last edited: 9/19/2018 by Drakonkinst
+ * */
+
 var Logger = {
+    /*
+     * Controls whether the other functions will print anything
+     * when called. This predicate can be changed to wherever the
+     * debug boolean is stored for easy modification.
+     * */
     isDebug: function() {
         return Game.options.debug;
     },
+
+    /*
+     * Standard logging - just like default console
+     * logging, but with the isDebug() condition
+     * */
     log: function(msg) {
         if(this.isDebug()) {
             console.log(msg);
         }
     },
-    logIf: function(condition, msg) {
-        if(condition && this.isDebug()) {
-            this.log(msg);
-        }
-        return condition;
-    },
+
     warn: function(msg) {
         if(this.isDebug()) {
             console.warn(msg);
         }
     },
-    warnIf: function(condition, msg) {
-        if(condition && this.isDebug()) {
-            this.warn(msg);
+
+    /*
+     * Conditional logging - only logs if the condition
+     * is met and isDebug() is true. Also returns whether
+     * the condition was met either way, so the condition does
+     * not have to be checked twice.
+     * 
+     * Note that by calling this wrapper class's method for
+     * standard logging, isDebug() does not need to be checked.
+     * */
+    logIf: function(condition, msg) {
+        if(condition) {
+            this.log(msg);
         }
         return condition;
     },
-}
+    
+    warnIf: function(condition, msg) {
+        if(condition) {
+            this.warn(msg);
+        }
+        return condition;
+    }
+};
