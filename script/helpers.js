d78eb2aa08d96b1c3594eb965a048b12dac1913e
Drakonkinst <drakonkinstmc@gmail.com>
2018-10-03 23:24:55 -0700

    Added formatting and comments for romanize()
    - Neatly spaced the lookup table for the aesthetic
    - Added method description

--- script/helpers.js
+++ script/helpers.js
@@ -91,21 +91,22 @@ function chooseWeighted(choiceMap, property) {
     Logger.warn("No choice found");
 }
 
+//returns a number in roman numerals
 function romanize(num) {
     var lookup = {
         M: 1000,
         CM: 900,
-        D: 500,
+        D:  500,
         CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1
+        C:  100,
+        XC:  90,
+        L:   50,
+        XL:  40,
+        X:   10,
+        IX:   9,
+        V:    5,
+        IV:   4,
+        I:    1
     };
 
     var roman = "";
