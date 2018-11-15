dae48effef03aa639a75e0b36ff30ce694da7419
Drakonkinst <drakonkinstmc@gmail.com>
2018-11-15 15:59:30 -0800

    Updated button.js
    - Added/rewrote documentation
    - onFinish() is now undefined if it is not specified in the Button's constructor properties

--- script/button.js
+++ script/button.js
@@ -1,17 +1,18 @@
 /*
  * Button class that represents a clickable button in the window.
  * Buttons can have text inside of them, a cooldown before it can be
- * clicked again, an action when it is clicked, and an action after the
- * button has cooled down.
+ * clicked again, an action when it is clicked, a tooltip displayed on
+ * hover, and an action after the button has cooled down.
  * 
- * properties:
- * - String id: button's id (default "btn_undefined")
- * - String text: text inside button element (default "press me")
- * - int cooldown: cooldown of button in milliseconds (default 0)
- * - Function onClick: function that runs when the button is clicked (default empty)
- * - Function onFinish: function that runs when the button finished cooling down (default empty)
- * - (optional) Tooltip tooltip: button tooltip
- * - (optional) int width: special width of button
+ * properties {
+ *      id: id of Button (default "btn_undefined")
+ *      cooldown: cooldown of Button in milliseconds (default 0)
+ *      text: Button text (default "press me")
+ *      onClick: function to run when Button is clicked (default warning)
+ *      onFinish: function to run when the Button finishes cooling down (optional)
+ *      tooltip: Tooltip shown when hovering over Button (optional)
+ *      width: custom width of button (px)
+ * }
  * */
 function Button(properties) {
     this.id = properties.id || "btn_undefined";
@@ -19,19 +20,21 @@ function Button(properties) {
     this.onCooldown = false;
     var text = properties.text || "press me";
 
-    //create element
+    //create Button element
     this.element = $("<div>")
         .attr("id", this.id)
         .addClass("button")
         .text(text)
         .click(function() {
+            //convert element to jQuery object
             var el = $(this);
+
             if(!el.hasClass("disabled")) {
                 //uses a callback to find the button object from the element's id, then clicks it
                 var button = Buttons.getButton(el.attr("id"));
                 var result = button.onClick();
 
-                //if result does not exist or returns true, begins the button cooldown
+                //if result returns true or undefined, click treated as "successful" - begins the button cooldown
                 if(isUndefined(result) || result) {
                     button.startCooldown();
                 }
@@ -39,13 +42,12 @@ function Button(properties) {
         })
         .append($("<div>").addClass("cooldown"));
 
-
-    //adds tooltip if it exists
+    //adds Tooltip if it exists
     if(!isUndefined(properties.tooltip) && properties.tooltip.exists()) {
         properties.tooltip.appendTo(this.element);
     }
 
-    //modifies width if special width exists
+    //modifies width if custom width exists
     if(!isUndefined(properties.width)) {
         this.element.css("width", properties.width);
     }
@@ -54,9 +56,7 @@ function Button(properties) {
     this.onClick = properties.onClick || function() {
         Logger.warn("Button \"" + this.id + "\" clicked but nothing happened");
     };
-    this.onFinish = properties.onFinish || function() {
-        //empty function
-    };
+    this.onFinish = properties.onFinish;
     
     //registers button for easier callbacks later
     Buttons.buttons[this.id] = this;
@@ -73,7 +73,8 @@ Button.prototype = {
             cooldown /= 4;
         }
 
-        if(cooldown <= 2) {
+        //avoids perma-disabled bug by treating cooldown as instant
+        if(cooldown <= 2 && !isUndefined(this.onFinish)) {
             this.onFinish();
             return;
         }
@@ -81,11 +82,14 @@ Button.prototype = {
         //makes sure the button isn't already in the middle of a cooldown
         this.clearCooldown();
 
+        //animates button
         this.element.find(".cooldown").width("100%").animate({width:"0%"}, cooldown, "linear", function() {
             //callbacks are fun
             var button = Buttons.getButton($(this).parent().attr("id"));
             button.clearCooldown();
-            button.onFinish();
+            if(!isUndefined(button.onFinish)) {
+                button.onFinish();
+            }
         });
         this.onCooldown = true;
         this.setDisabled(true);
@@ -133,8 +137,7 @@ Button.prototype = {
  * Button objects, allows for easier callbacks and less headaches
  * */
 var Buttons = {
-    //keymap of all button ids and their associated buttons
-    //TODO: each module should have their own button keymap
+    //keymap of Button ids and their associated Button objects
     buttons: {},
 
     //returns a Button object based on requested id
