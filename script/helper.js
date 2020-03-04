c004b6284116e29e8b33d06de2ef1a7b6a6a6754
Drakonkinst <drakonkinstmc@gmail.com>
2020-03-03 22:04:58 -0800

    Added isUndefined and getOrDefault

--- script/helper.js
+++ script/helper.js
@@ -2,3 +2,14 @@
 function chooseRandom(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
+
+function isUndefined(variable) {
+    return typeof variable === "undefined" || variable === null;
+}
+
+function getOrDefault(value, defaultValue) {
+    if(isUndefined(value)) {
+        return defaultValue;
+    }
+    return value;
+}
