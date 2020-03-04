1517210a2cd5d31f7fed1049ea85501042916291
Drakonkinst <drakonkinstmc@gmail.com>
2020-03-03 20:25:29 -0800

    Added documentation for Logger

--- script/logger.js
+++ script/logger.js
@@ -1,23 +1,64 @@
+/**
+ * Logger object that wraps the default console object so that
+ * debug messages are only printed when a certain boolean is true.
+ */
+
 var Logger = {
 
+    /**
+     * Returns whether messages should be outputted to console, based on a
+     * certain boolean.
+     * 
+     * @return {boolean} Whether log messages should be outputted.
+     */
     isDebug: function() {
         return Game.options.debug;
     },
 
+    /**
+     * Logs a message to the console.
+     * 
+     * @param {String} msg The string to output to console. 
+     */
     log: function(msg) {
         if(this.isDebug()) {
             console.log(msg);
         }
     },
 
+    /**
+     * Logs a message only if the condition is true.
+     * Returns whether the condition is true or false.
+     * 
+     * @param {boolean} condition The condition to check.
+     * @param {String} msg The string to output to console. 
+     * @return the condition
+     */
     logIf: function(condition, msg) {
         
     },
 
+    /**
+     * Logs a warning message to the console.
+     * 
+     * @param {String} msg The string to output to console. 
+     */
     warn: function(msg) {
         if(this.isDebug()) {
             console.warn(msg);
         }
+    },
+
+    /**
+     * Logs a warning message only if the condition is true.
+     * Returns whether the condition is true or false.
+     * 
+     * @param {boolean} condition The condition to check.
+     * @param {String} msg The string to output to console. 
+     * @return the condition
+     */
+    warnIf: function(condition, msg) {
+
     }
 
 }
